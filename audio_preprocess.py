9c30ddfbac7e76c5fe094924b2a8d582ffdf6274
kishorkumarb2024-crypto <kishorkumar.b2024@vitstudent.ac.in>
2025-08-27 17:34:00 +0530

    first update

--- audio_preprocess.py
+++ audio_preprocess.py
@@ -1,38 +1,94 @@
-import librosa
 import soundfile as sf
+
 import numpy as np
 
-# Load the audio file
-audio, sr = librosa.load('data/aud_test.wav', sr=None)
-print(f"Original audio shape: {audio.shape}")
-print(f"Original sample rate: {sr}")
+import matplotlib.pyplot as plt
 
-# Convert to mono if it's stereo (librosa.load does this by default, but just in case)
-if audio.ndim > 1:
-    audio = np.mean(audio, axis=1)
+import librosa
+import librosa.display
 
-# Normalize audio to range [-1, 1]
-audio = audio / np.max(np.abs(audio))
+def load_raw_data():
+    # Load the audio file
+    audio, sr = librosa.load('data/aud_test.wav', sr=None)
+    print(f"Original audio shape: {audio.shape}")
+    print(f"Original sample rate: {sr}")
 
-# Optional: RMS normalization to control loudness (set to target RMS = 0.1)
-rms = np.sqrt(np.mean(audio**2))
-audio = audio / (rms / 0.1)
+    return audio, sr
 
-# Save the cleaned/normalized audio
-sf.write('cleaned/aud_test_clean.wav', audio, sr)
-print("✅ Cleaned audio saved to 'cleaned/aud_test_clean.wav'")
+def mono(audio):
+    # Convert to mono if it's stereo (librosa.load does this by default, but just in case)
+    if audio.ndim > 1:
+        audio = np.mean(audio, axis=1)
 
-# Optional: Reload and verify
-audio_clean, sr_clean = librosa.load('cleaned/aud_test_clean.wav', sr=None)
-print(f"Cleaned audio shape: {audio_clean.shape}")
-print(f"Cleaned sample rate: {sr_clean}")
+    # Normalize audio to range [-1, 1]
+    audio = audio / np.max(np.abs(audio))
+    return audio
 
-# ✅ Compare stats
-print(f"\n📊 Amplitude Comparison:")
-print(f"Original max amplitude: {np.max(np.abs(audio))}")
-print(f"Cleaned max amplitude: {np.max(np.abs(audio_clean))}")
+def normalize(audio):
+    # Optional: RMS normalization to control loudness (set to target RMS = 0.1)
+    rms = np.sqrt(np.mean(audio**2))
+    audio = audio / (rms / 0.1)
 
-rms_orig = np.sqrt(np.mean(audio**2))
-rms_clean = np.sqrt(np.mean(audio_clean**2))
-print(f"Original RMS: {rms_orig:.4f}")
-print(f"Cleaned RMS: {rms_clean:.4f}")
+    return audio
+
+def save(audio, source):
+    # Save the cleaned/normalized audio
+    sf.write('cleaned/aud_test_clean.wav', audio, source)
+    print("✅ Cleaned audio saved to 'cleaned/aud_test_clean.wav'")
+
+def load():
+    # Optional: Reload and verify
+    audio_clean, sr_clean = librosa.load('cleaned/aud_test_clean.wav', sr=None)
+    print(f"Cleaned audio shape: {audio_clean.shape}")
+    print(f"Cleaned sample rate: {sr_clean}")
+
+    return audio_clean, sr_clean
+
+def compare(raw, clean):
+    # ✅ Compare stats
+    print(f"\n📊 Amplitude Comparison:")
+    print(f"Original max amplitude: {np.max(np.abs(raw))}")
+    print(f"Cleaned max amplitude: {np.max(np.abs(clean))}")
+
+    rms_orig = np.sqrt(np.mean(raw**2))
+    rms_clean = np.sqrt(np.mean(clean**2))
+    print(f"Original RMS: {rms_orig:.4f}")
+    print(f"Cleaned RMS: {rms_clean:.4f}")
+
+def plot(mag_db):
+    plt.figure(figsize=(12, 6)) # sizing the image so that the dimensions of the plot-image is 12x6
+    librosa.display.specshow(mag_db, sr=sr, hop_length=512, x_axis="time", y_axis="log")
+    plt.title("Spectrogram")
+    plt.show()
+
+def stft(audio):
+    """
+    The objective of computational STFT is to divide a longer time signal into shorter segments of equal length and then
+    compute the Fourier transform separately on each shorter segment
+    """
+    data = librosa.stft(audio, n_fft=2048, hop_length=512, win_length=2048)
+    mag, phase = librosa.magphase(data)
+
+    mag_db = librosa.amplitude_to_db(np.abs(data), ref=np.max)
+    plot(mag_db)
+
+    return data
+
+def istft(data, sr):
+    recon = librosa.istft(data, hop_length=512, win_length=2048)
+    save_reconstructed(recon, sr)
+
+def save_reconstructed(data, sr):
+    sf.write("recon.wav", data, sr)
+
+
+if __name__ == "__main__":
+    audio_raw, sr = load_raw_data()
+    audio_raw = mono(audio_raw)
+    audio_raw = normalize(audio_raw)
+
+    # save(audio, sr)
+    audio_clean, sr = load()
+    compare(audio_raw, audio_clean)
+    decon = stft(audio_raw)
+    istft(decon, sr)
